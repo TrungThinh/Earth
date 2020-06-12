10c1e6835f980082953d6161bd6c0b1f3db8d6d2
quanghotrungthinh <quanghotrungthinh@gmail.com>
2020-06-12 13:14:44 +0700

    add unittest

---
+++ test/Data/Earth.Test.Data.IO/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Earth.Test.Data.IO
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
